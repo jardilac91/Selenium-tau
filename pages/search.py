67dc50980ae2832531001af19b55eb618ba2d47b
jorge <jardilac91@gmail.com>
2022-03-23 17:02:23 -0500

    Clase 6 - Webdriver calls

--- pages/search.py
+++ pages/search.py
@@ -3,19 +3,26 @@ Este módulo contiene DuckDuckGoSearchPage,
 la vista encargada de representar la página de búsqueda de DuckDuckGo
 """
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class DuckDuckGoSearchPage:
 
+    # URL
+    URL = 'https://www.duckduckgo.com'
+
+    # Locators
     SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
 
+    # Initializer
     def __init__(self,browser):
         self.browser = browser
 
+    # Interaction Methods
+
     def load(self):
-        # TODO
-        pass
+        self.browser.get(self.URL)
 
     def search(self, phrase):
-        # TODO
-        pass
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
